4e463ccaeabe40b09058e16f5e4485709e898ca5
Prince-Kd <34554163+Prince-Kd@users.noreply.github.com>
2018-09-13 10:06:29 +0000

    Add files via upload

---
+++ a4_10667819.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+ 
+using namespace std;
+int gcd(int a, int b)
+{
+    return (b != 0) ? gcd(b, a % b) : a;
+}
+ 
+int main(void)
+{
+    int num1, num2, result;
+    cout << "Enter two numbers: ";
+    cin >> num1 >>endl;
+    cin >> num2 >>endl;
+    result = gcd(num1, num2);
+    if (gcd)
+        cout << "\nThe GCD of " << num1 << " and " << num2 << " is: " << result
+                << endl;
+    else
+        cout << "\nInvalid input!!!\n";
+    return 0;
+}
